e889908b622079aa1c3648f03785987be2b53988
Naeku72 <naekujoy@gmail.com>
2018-06-22 21:33:09 +0300

    Added link to pages on types of breast cancer

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 </head>
 <body>
   <div id="wrapper">
-    <h2>Log in to my forum | Breast Cancer Support forum</h2>
-    <p>Creating Basic log in function</p>
+    <h2>Support Breast Cancer</h2>
+    <h3>Welcome to Breast Cancer Support Forum</h3>
+
     <?php
     if (!isset($_SESSION['uid'])){
       echo "<form action='login_parse.php' method='post'>
@@ -22,10 +23,40 @@
     }
     ?>
   </div>
+  <div id="home">
+    <p>Breast cancer in Kenya is the third most common cause of death after infectious and cardiovascular diseases. It contributes to 23.3% of cancer deaths. There are many factors that increase the chances of developing breast cancer. Although we know some of the risk factors, the main cause of breast cancer like any other cancer is not known. In Africa breast cancer kills more women over age 40 than HIV&AIDS and malaria combined. Five-year survival rates for breast cancer are much lower/worse in low and middle income countries.</p>
+
+    <p>Our main aim is ro provide support for our users; breast cancer patients, survivors, doctors and other parties who have been affected by breast cancer one way or the other. Any other person(s) interested in supporting our users can feel free to join us</p>
+    <br>
+    <p>Disclaimer: Statements or sentiments that are aimed at demeaning or demoralising our users will not be tolerated</p>
+
+    <p>Click here to read our <a href="#">Terms & Conditions</a> and our <a href="#">Privacy Policy</a>.</p>
+    <br>
+    <h2>Types of breast cancer.</h2>
+    <p>Breast cancer can begin in different areas of the breast — the ducts, the lobules, or in some cases, the tissue in between. In this section, you can learn about the different types of breast cancer, including non-invasive, invasive, and metastatic breast cancers, as well as the intrinsic or molecular subtypes of breast cancer. You can also read about breast cancer in men. </p>
+    <div class="types">
+      <a href="dcis.html">Ductal Carcinoma In Situ (DCIS)</a>
+      <a href="idc.html">Invasive Ductal Carcinoma (IDC)</a>
+      <a href="tcb.html">IDC Type: Tubular Carcinoma of the Breast</a>
+      <a href="mcb.html">IDC Type: Medullary Carcinoma of the Breast</a>
+      <a href="mucb.html">IDC Type: Mucinous Carcinoma of the Breast</a>
+      <a href="pcb.html">IDC Type: Papillary Carcinoma of the Breast</a>
+      <a href="ccb.html">IDC Type: Cribriform Carcinoma of the Breast</a>
+      <a href="ilc.html">Invasive Lobular Carcinoma (ILC)</a>
+      <a href="ibc.html">Inflammatory Breast Cancer</a>
+      <a href="lcis.html">Lobular Carcinoma In Situ (LCIS)</a>
+      <a href="mbc.html">Male Breast Cancer</a>
+      <a href="mosbc.html">Molecular Subtypes of Breast Cancer</a>
+      <a href="pdn.html">Paget's Disease of the Nipple</a>
+      <a href="ptb.html">Phyllodes Tumors of the Breast</a>
+      <a href="mebc.html">Metastatic Breast Cancer</a>
+    </div>
+  </div>
 <br>
 <hr />
 <br>
 <div id="content">
+  <h3>Below is a list of some categories we have where our members are able to discuss issues relatinf to breast cancer, especially in Kenya</h3>
 <?php
 include_once("connect.php");
 $sql = "SELECT * FROM categories ORDER BY category_title ASC";
--- post_reply_parse.php
+++ post_reply_parse.php
@@ -14,38 +14,6 @@ if ($_SESSION['uid']){
     $sql3 = "UPDATE topics SET topic_reply_date=now(), topic_last_user='".$creator."' WHERE id='".$tid."' LIMIT 1";
     $res3 = mysqli_query($con, $sql3) or die(mysqli_error($con));
 
-    //Email Sending
-    $sql4 = "SLECT post_creator FROM posts WHERE category_id='".$cid."' AND topic_id='".$tid."' GROUP BY post_creator";
-    $res4 = mysqli_query($con, $sql3) or die(mysqli_error($con));
-    while ($row4 = mysqli_fetch_assoc($res4)){
-      $userids[] .= $row4['post_creator'];
-    }
-    foreach ($useerids as $key) {
-      $sql5 = "SELECT id, email FROM users WHERE id='".$key."' AND forum_notification='1' LIMIT 1";
-      $res5 = mysqli_query($con, $sql5) or die(mysqli_error($con));
-      if (mysqli_num_rows($res5)> 0) {
-          $row5 = mysqli_fetch_assoc($res5);
-          if ($row5['id'] != $creator){
-            $email .= $row5['email'].", ";
-          }
-      }
-    }
-    $email = substr($email, 0, (strlen(email) -2));
-    $to = "naekujoy@gmail.com";
-    $from = "naekujoy@gmail.com";
-    $bcc = $email;
-    $subject = "Forum Reply";
-
-    //This message can only contain text. No HTML tags! The HTML tags will just be printed into the email.
-    $message = "Someon has replied to a topic you are a part of";
-
-    $headers = "From: $from\r\nReply-To: $from";
-    $headers .= "\r\nBcc: {$bcc}";
-    mail($to, $subject, $message, $headers);
-    //End of EMAIL sending
-
-
-
     if(($res) && ($res2) && ($res3)) {
       echo "<p>Your reply has been successfully posted. <a href='view_topic.php?cid=".$cid."&tid=".$tid."'>Click here to return to the topic</a></p>";
     } else {
